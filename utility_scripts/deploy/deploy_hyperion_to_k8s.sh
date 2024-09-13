794469f578231bd71deff306eb00c153c209421e
rtuck99 <robert.tuck@diamond.ac.uk>
2024-09-13 11:17:45 +0100

    Fixes to kubernetes deployment helmcharts (#468)
    * Add data dir to helmchart, make log dir writable, fix project dir version prefix
    
    * Simplify helmchart conditionals into values.yaml

--- utility_scripts/deploy/deploy_hyperion_to_k8s.sh
+++ utility_scripts/deploy/deploy_hyperion_to_k8s.sh
@@ -160,12 +160,13 @@ hyperion.dev=true,\
 hyperion.runAsUser=$EUID,\
 hyperion.runAsGroup=$GID,\
 hyperion.supplementalGroups=[$SUPPLEMENTAL_GIDS],\
-hyperion.logDir=/app/hyperion/tmp,\
+hyperion.logDir=$PROJECTDIR/tmp,\
+hyperion.dataDir=$PROJECTDIR/tmp/data,\
 hyperion.externalHostname=test-hyperion.diamond.ac.uk "
-  mkdir -p $PROJECTDIR/tmp
+  mkdir -p $PROJECTDIR/tmp/data
   DEPLOYMENT_DIR=$PROJECTDIR
 else
-  DEPLOYMENT_DIR=/dls_sw/i03/software/bluesky/mx_bluesky_v${APP_VERSION}/hyperion
+  DEPLOYMENT_DIR=/dls_sw/i03/software/bluesky/mx_bluesky_${APP_VERSION}/hyperion
 fi
 
 HELM_OPTIONS+="--set hyperion.appVersion=$APP_VERSION,\
