113401817b3009b714197b1c747755e0e48a9526
rtuck99 <robert.tuck@diamond.ac.uk>
2024-11-07 14:17:30 +0000

    Remove redundant unguarded workaround for pytest GH issue 7409 (#624)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import asyncio
 import gzip
 import json
 import logging
+import os
 import sys
 import threading
 from collections.abc import Callable, Generator, Sequence
@@ -951,16 +952,19 @@ def pin_tip_edge_data():
     return tip_x_px, tip_y_px, top_edge_array, bottom_edge_array
 
 
-@pytest.hookimpl(tryfirst=True)
-def pytest_exception_interact(call: pytest.CallInfo[Any]):
-    if call.excinfo is not None:
-        raise call.excinfo.value
-    else:
-        raise RuntimeError(
-            f"{call} has no exception data, an unknown error has occurred"
-        )
+# Prevent pytest from catching exceptions when debugging in vscode so that break on
+# exception works correctly (see: https://github.com/pytest-dev/pytest/issues/7409)
+if os.getenv("PYTEST_RAISE", "0") == "1":
 
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_exception_interact(call: pytest.CallInfo[Any]):
+        if call.excinfo is not None:
+            raise call.excinfo.value
+        else:
+            raise RuntimeError(
+                f"{call} has no exception data, an unknown error has occurred"
+            )
 
-@pytest.hookimpl(tryfirst=True)
-def pytest_internalerror(excinfo: pytest.ExceptionInfo[Any]):
-    raise excinfo.value
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_internalerror(excinfo: pytest.ExceptionInfo[Any]):
+        raise excinfo.value
--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,21 +1,8 @@
 import asyncio
-import os
 import time
 
 import pytest
 from bluesky.run_engine import RunEngine
-
-# Prevent pytest from catching exceptions when debugging in vscode so that break on
-# exception works correctly (see: https://github.com/pytest-dev/pytest/issues/7409)
-if os.getenv("PYTEST_RAISE", "0") == "1":
-
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_exception_interact(call):
-        raise call.excinfo.value
-
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_internalerror(excinfo):
-        raise excinfo.value
 
 
 @pytest.fixture
